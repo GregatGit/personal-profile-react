7cef1dbc91058b191f96a55698919a02636d7317
gregatgit <gregdd@outlook.com>
2019-07-12 12:24:13 +1000

    Updates

---
+++ precache-manifest.137e6aa461c01551411276ad80decf82.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e51842d532937274598f443c1c5958d",
+    "url": "/personal-profile-react/index.html"
+  },
+  {
+    "revision": "7e6b4c8860c1bc54be04",
+    "url": "/personal-profile-react/static/js/2.ea26fb1e.chunk.js"
+  },
+  {
+    "revision": "2c8898f34bd70a5a8291",
+    "url": "/personal-profile-react/static/js/main.af78134c.chunk.js"
+  },
+  {
+    "revision": "f38cfe3c3a219b90535f",
+    "url": "/personal-profile-react/static/js/runtime~main.218f9d76.js"
+  },
+  {
+    "revision": "3b300187e123f56ca2e1c5fc6a748ae9",
+    "url": "/personal-profile-react/static/media/GameOfLife.3b300187.JPG"
+  },
+  {
+    "revision": "feace2c876e8b4ca66a532bdeca98afb",
+    "url": "/personal-profile-react/static/media/Sunny1.feace2c8.jpg"
+  },
+  {
+    "revision": "e83164e65a03d24ee108161d33e96668",
+    "url": "/personal-profile-react/static/media/calculator.e83164e6.JPG"
+  },
+  {
+    "revision": "bdadccafe6b454891b4cb71cd9b200a8",
+    "url": "/personal-profile-react/static/media/greg.bdadccaf.jpg"
+  },
+  {
+    "revision": "4769959fc58b667575960cd67adba25b",
+    "url": "/personal-profile-react/static/media/iron-throne.4769959f.jpg"
+  },
+  {
+    "revision": "c937b25cda6aab270a2525f6fa350e85",
+    "url": "/personal-profile-react/static/media/markdown-previewer.c937b25c.JPG"
+  },
+  {
+    "revision": "3b61f4cd3519d719e711650d2bfd8c55",
+    "url": "/personal-profile-react/static/media/pomodoro.3b61f4cd.JPG"
+  },
+  {
+    "revision": "70a48e403f0acc291bc5662dcfdb4eeb",
+    "url": "/personal-profile-react/static/media/projects.70a48e40.JPG"
+  },
+  {
+    "revision": "2434a9c47343a9f6bd6a7c7ce84b67af",
+    "url": "/personal-profile-react/static/media/quote.2434a9c4.JPG"
+  },
+  {
+    "revision": "0705e9dbe4002551358ba588fda0d4e3",
+    "url": "/personal-profile-react/static/media/recipebox.0705e9db.JPG"
+  },
+  {
+    "revision": "af9c12c3f2453c46f93430f8fbeddefe",
+    "url": "/personal-profile-react/static/media/simon-game.af9c12c3.JPG"
+  },
+  {
+    "revision": "c4206fb79db049a856119648e70a5353",
+    "url": "/personal-profile-react/static/media/tictactoe.c4206fb7.JPG"
+  },
+  {
+    "revision": "e410d5bf4a754551651455795194f9f5",
+    "url": "/personal-profile-react/static/media/wiki.e410d5bf.JPG"
+  }
+]);
