a74514ab2de74c027d5bb3b57788695dbaa6e5c0
gregatgit <gregdd@outlook.com>
2019-10-14 11:58:31 +1000

    comment out unused code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { createMuiTheme } from '@material-ui/core'
-import {red, amber } from '@material-ui/core/colors'
+// import { createMuiTheme } from '@material-ui/core'
+// import {red, amber } from '@material-ui/core/colors'
 
-const theme = createMuiTheme({
-  palette: {
-    primary: red,
-    secondary: {
-      main: amber[500],
-      // light: ,
-      // dark: 
-    },
-    type: 'light'
-  }
-})
-
-console.log(theme)
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: red,
+//     secondary: {
+//       main: amber[500],
+//       // light: ,
+//       // dark: 
+//     },
+//     type: 'light'
+//   }
+// })
 
 ReactDOM.render(
   //<MuiThemeProvider theme={theme}>
